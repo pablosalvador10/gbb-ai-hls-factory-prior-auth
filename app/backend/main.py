611513b39273e408f58cbcf12faff90ea5aa6e73
Joe Karasha <jkarasha@gmail.com>
2025-01-22 16:46:42 -0800

    Add fastapi-user support.

---
+++ app/backend/main.py
@@ -0,0 +1,81 @@
+import time
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+import sentry_sdk
+#
+from beanie import init_beanie
+from fastapi import FastAPI, Request, Depends
+from starlette.middleware.cors import CORSMiddleware
+#
+from .core.database import User, db
+#
+from app.backend.core.config import settings, app_configs
+from .users.schemas import UserCreate, UserRead, UserUpdate
+from .users.manager import auth_backend, current_active_user, fastapi_users
+
+@asynccontextmanager
+async def lifespan(_application: FastAPI) -> AsyncGenerator:
+    # startup
+    await init_beanie(db, document_models=[User])
+    yield
+    # shutdown
+
+app = FastAPI(**app_configs, lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.CORS_ORIGINS,
+    allow_origin_regex=settings.CORS_ORIGINS_REGEX,
+    allow_credentials=True,
+    allow_methods=("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"),
+    allow_headers=settings.CORS_HEADERS,
+)
+
+if settings.ENVIRONMENT.is_deployed:
+    sentry_sdk.init(
+        dsn=settings.SENTRY_DSN,
+        environment=settings.ENVIRONMENT,
+    )
+
+# middleware test
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend), prefix="/auth/jwt", tags=["auth"]
+)
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_reset_password_router(),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_verify_router(UserRead),
+    prefix="/auth",
+    tags=["auth"],
+)
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+@app.get("/authenticated-route")
+async def authenticated_route(user: User = Depends(current_active_user)):
+    return {"message": f"Hello {user.email}!"}
+
+@app.get("/healthcheck", include_in_schema=False)
+async def healthcheck() -> dict[str, str]:
+    #
+    return {"status": "We are doing ok!"}
